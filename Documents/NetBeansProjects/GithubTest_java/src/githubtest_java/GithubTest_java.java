3471cfd10a1b16e4a3d4a28abfe576483ba47d90
SandaruwanDissanayake <sandaruwandissanayake9@gmail.com>
2024-05-19 10:22:12 +0530

    Version 1.0

---
+++ Documents/NetBeansProjects/GithubTest_java/src/githubtest_java/GithubTest_java.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubtest_java;
+
+import java.awt.Event;
+
+/**
+ *
+ * @author Sandaruwan
+ */
+public class GithubTest_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
